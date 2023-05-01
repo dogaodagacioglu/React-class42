d37ca9547c026bd513740132064170693372af9d
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-05-01 15:06:01 +0300

    add productdetail companent

---
+++ week2/project/ecommerce/src/component/productDetails.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const ProductsDetails = ({ product }) => {
+    return (
+    <div className='root'>
+      <div className='product-details'>
+        <div className='title-container'>
+            <h1 className='title-container--title'>s</h1>
+        </div>
+        <div className='product-details--information'>
+            <div className='product-details--image'>
+                <div className='product-image-container'>
+                    <img className='product-image' src=''></img>
+                </div>
+            </div>
+            <p className='product-details--description'></p>
+        </div>
+      </div>
+    </div>
+    );
+  };
